ab91930b90453b1aca1dde6573b6c773926af588
Luc Engelen <luc@kabisa.nl>
2019-03-03 17:13:00 +0100

    Provide feedback in case of errors

--- cli.js
+++ cli.js
@@ -3,19 +3,35 @@
 const childProcess = require("child_process");
 const fs = require("fs");
 
-const symbolicRef = childProcess.execSync("git symbolic-ref --short HEAD").toString().trim();
+const scriptName = "prepare-commit-id";
+
+let symbolicRef;
+try {
+  symbolicRef = childProcess.execSync("git symbolic-ref --short HEAD").toString().trim();
+} catch (e) {
+  console.error(scriptName + " doesn't work without Git.");
+  process.exit(1);
+}
+
 const match = symbolicRef.match(/[^/]*\/(([A-Z]*-)?[0-9]*).*/);
+if (!match) {
+  process.exit();
+}
 
-if (match) {
-  const identifier = match[1];
+const identifier = match[1];
+if (!identifier) {
+  process.exit();
+}
 
-  if (identifier) {
-    const commitMessageFile = process.argv[2];
-    const content = fs.readFileSync(commitMessageFile);
-    const prefix = "[ " + identifier + " ] ";
+const commitMessageFile = process.argv[2];
+if (!commitMessageFile) {
+  console.error(scriptName + " requires the name of the file containing the commit log message as first argument.");
+  process.exit(1);
+}
 
-    if (content.indexOf(prefix) === -1) {
-      fs.writeFileSync(commitMessageFile, prefix + content);
-    }
-  }
+const content = fs.readFileSync(commitMessageFile);
+const prefix = "[ " + identifier + " ] ";
+
+if (content.indexOf(prefix) === -1) {
+  fs.writeFileSync(commitMessageFile, prefix + content);
 }
